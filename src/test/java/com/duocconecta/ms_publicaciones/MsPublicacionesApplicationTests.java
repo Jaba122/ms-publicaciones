2c75fcd3b4585799384468cf64b4df2864dbb174
Jaba122 <ja.nunezo@duocuc.cl>
2026-08-27 14:41:27 -0400

    inicio proyecto

---
+++ src/test/java/com/duocconecta/ms_publicaciones/MsPublicacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duocconecta.ms_publicaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPublicacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
