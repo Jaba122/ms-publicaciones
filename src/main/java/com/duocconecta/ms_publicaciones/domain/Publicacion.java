dac41167c71d688033155a3cb80b5b775e50ff58
Jaba122 <ja.nunezo@duoc.cl>
2026-09-02 00:11:15 -0400

    estructura inicial

---
+++ src/main/java/com/duocconecta/ms_publicaciones/domain/Publicacion.java
@@ -0,0 +1,5 @@
+package com.duocconecta.ms_publicaciones.domain;
+
+public class Publicacion {
+    
+}
---
+++ src/main/java/com/duocconecta/ms_publicaciones/service/PublicacionService.java
@@ -0,0 +1,5 @@
+package com.duocconecta.ms_publicaciones.service;
+
+public class PublicacionService {
+    
+}
